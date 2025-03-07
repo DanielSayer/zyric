7b55884c8516cb63501c29d74975380dfb490cc9
Daniel Sayer <daniel.sayer01@gmail.com>
2025-03-07 12:19:31 +1000

    feat: add sync on nav

--- src/components/editor/useSyncOnUnmount.ts
+++ src/components/editor/useSyncOnUnmount.ts
@@ -1,26 +1,41 @@
 import type { IDBLessonPlan } from "@/lib/db/db";
 import indexDb from "@/lib/db/db";
-import { useCallback, useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, useRef } from "react";
 
 type UseSyncOnUnmountProps = {
   lessonPlan: Omit<IDBLessonPlan, "updatedAt">;
 };
 
 export const useSyncOnUnmount = ({ lessonPlan }: UseSyncOnUnmountProps) => {
-  const saveToIndexedDB = useCallback(() => {
-    console.log("saving to indexedDB");
-    return indexDb.table("lessonPlans").put({
-      id: lessonPlan.id,
-      title: lessonPlan.title ?? "",
-      backgroundId: lessonPlan.backgroundId,
-      content: lessonPlan.content,
-      updatedAt: new Date().toISOString(),
-    });
+  const saveToIndexDbRef = useRef<() => void>();
+  const pathname = usePathname();
+  const params = useSearchParams();
+
+  useEffect(() => {
+    saveToIndexDbRef.current = () => {
+      indexDb
+        .table("lessonPlans")
+        .put({
+          id: lessonPlan.id,
+          title: lessonPlan.title ?? "",
+          backgroundId: lessonPlan.backgroundId,
+          content: lessonPlan.content,
+          updatedAt: new Date().toISOString(),
+        })
+        .catch(console.error);
+    };
   }, [lessonPlan]);
 
   useEffect(() => {
+    return () => {
+      saveToIndexDbRef.current?.();
+    };
+  }, [pathname, params]);
+
+  useEffect(() => {
     const handleBeforeUnload = () => {
-      saveToIndexedDB().catch(console.error);
+      saveToIndexDbRef.current?.();
 
       // This ensures the above runs
       // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -32,12 +47,12 @@ export const useSyncOnUnmount = ({ lessonPlan }: UseSyncOnUnmountProps) => {
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-  }, [lessonPlan.id, saveToIndexedDB]);
+  }, [lessonPlan.id]);
 
   useEffect(() => {
     const handleVisibilityChange = () => {
       if (document.visibilityState === "hidden") {
-        saveToIndexedDB().catch(console.error);
+        saveToIndexDbRef.current?.();
 
         // This ensures the above runs
         // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -50,5 +65,5 @@ export const useSyncOnUnmount = ({ lessonPlan }: UseSyncOnUnmountProps) => {
     return () => {
       window.removeEventListener("visibilitychange", handleVisibilityChange);
     };
-  }, [lessonPlan.id, saveToIndexedDB]);
+  }, [lessonPlan.id]);
 };
